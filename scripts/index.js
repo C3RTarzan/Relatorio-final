1e5abc5535de61830cb7ee1ea6d918e19b0c4437
C3RTarzan <allanhivdc@gmail.com>
2024-08-05 19:32:08 -0300

    Creating first data dealings v0.2

--- scripts/index.js
+++ scripts/index.js
@@ -6,6 +6,19 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
+//@ STATE
+/*
+    "RECEBIDO CD DE: - FOR"
+    "EM ROTA"
+    "DESCARREGADO"
+    "ENTREGA REALIZADA"
+    "PROCESSO DE ENTREGA"
+    "EM TRANSFERÊNCIA SECUNDÁRIA"
+    "EMBARQUE RECEBIDO NO CD: - FOR"
+    "RECEBIDO CD DE: - - FOR"
+    "RETIRADO DA CAF ANTERIOR" 
+ */
+
 //: ----- Users name menu -----
 function configOpen() {
     const config = document.querySelector(".config")
@@ -139,13 +152,20 @@ function selectName() {
 function getData() {
 
     const mainData = document.querySelector("#data1").value;
-    const ticketsData = document.querySelector("#data2").value;
+    const cafData = document.querySelector("#data2").value;
+    const ticketsData = document.querySelector("#data3").value;
     
     const mainDataObject = createObjectData(mainData)
     const ticketsDataObject = createObjectTickets(ticketsData)
 
-    console.log(mainDataObject);
-    console.log(ticketsDataObject);
+    const toCheckTicket = TicketChecker(mainDataObject, ticketsDataObject)
+
+    const cafListMade = getCafs(toCheckTicket)
+
+    const sates = getStateAvailable(mainData) // Take status available
+    //console.log(sates);
+
+    console.log(cafListMade);
 }
 
 function createObjectData(data) {
@@ -172,12 +192,21 @@ function createObjectData(data) {
         throw new Error('Invalid input: data must be a string or an array of strings');
     }
 
+    // Check if the first line is a header
+    const header = data[0].split('\t');
+    const hasHeader = header[0].toLowerCase() === 'awb' && header.length >= 5;
+
+    // If there is a header, remove it
+    if (hasHeader) {
+        data.shift();
+    }
+
     // Turning data into validation objects
     return data.map(line => {
         const parts = line.split('\t');
         
-        // Check if the line has exactly 5 parts
-        if (parts.length !== 5) {
+        // Checks if the line has at least 6 parts (including the 'driver' field and the 'state' field prior to it)
+        if (parts.length < 5) {
             errorHandling(
                 "Os dados fornecidos no campo 'Dados Principais' são inválidos. Por favor, verifique e corrija as informações antes de enviar.",
                 new Error(`Invalid line format: ${line}`)
@@ -185,15 +214,17 @@ function createObjectData(data) {
             return null; // Returns null to indicate invalid line
         }
 
-        const [awb, router, boarding, caf, driver] = parts;
+        const [awb, router, boarding, caf, state, driver] = parts;
         return {
             AWB: awb,
             router: extractRouterValue(router),
             boarding: boarding,
             CAF: caf,
-            driver: driver
+            state: state,
+            driver: driver,
+            ticket: ""
         };
-    }).filter(item => item !== null); // Filters Invalid Items
+    }).filter(item => item !== null);
 }
 
 function createObjectTickets(data) {
@@ -207,6 +238,15 @@ function createObjectTickets(data) {
     // Check if date is an array
     if (!Array.isArray(data)) {
         throw new Error('Invalid input: data must be a string or an array of strings');
+    }
+
+    // Check if the first line is a header
+    const header = data[0].split('\t');
+    const hasHeader = header[0].toLowerCase() === 'awb' && header.length >= 2;
+
+    // If there is a header, remove it
+    if (hasHeader) {
+        data.shift();
     }
 
     // Turning data into validation objects
@@ -228,6 +268,115 @@ function createObjectTickets(data) {
             ticket: ticket 
         };
     }).filter(item => item !== null); // Filters Invalid Items
+}
+
+function TicketChecker(dataMain, dataTicket) {
+    // Itera about each item in Datamain
+    return dataMain.map(mainItem => {
+        // Finds the corresponding item in Dataticket with the same AWB
+        let ticketItem = false
+        if(!(dataTicket === '' || dataTicket === undefined)){
+            ticketItem = dataTicket.find(ticketItem => ticketItem.AWB === mainItem.AWB);
+        }
+
+        // If you found a corresponding item, check the ticket message
+        if (ticketItem) {
+            if (ticketItem.ticket.includes(`Encomenda auditada - 5 : ${mainItem.AWB}`)) {
+                mainItem.ticket = "audited";
+            } else {
+                mainItem.ticket = "notAuthenticated";
+            }
+        } else {
+            // If you have not found a corresponding item, it defines it as "Notauthenticated"
+            if(dataTicket === undefined || dataTicket === ''){
+                mainItem.ticket = "undefined" 
+            }else{
+                mainItem.ticket = "notAuthenticated";
+            }
+        }
+
+        return mainItem;
+    });
+}
+
+function getCafs(data) {
+    if (!Array.isArray(data)) {
+        throw new Error('Invalid input: data must be an array of objects');
+    }
+
+    const atTheBase = ["DESCARREGADO", "RECEBIDO CD DE: - FOR"];
+    const inCAF = ["EM ROTA", "EM TRANSFERÊNCIA SECUNDÁRIA"];
+    const inStreet = ["ENTREGA REALIZADA", "PROCESSO DE ENTREGA"];
+
+    const groupedData = data.reduce((acc, item) => {
+        const { AWB, router, boarding, CAF, state, driver, ticket } = item;
+
+        if (CAF === '0') return acc;
+
+        if (!acc[CAF]) {
+            acc[CAF] = {
+                AWBS: new Set(),
+                routers: {},
+                states: {},
+                drivers: new Set(),
+                boardings: new Set(),
+                ticket: ticket
+            };
+        }
+
+        acc[CAF].AWBS.add(AWB);
+
+        if (!acc[CAF].routers[router]) {
+            acc[CAF].routers[router] = 0;
+        }
+        acc[CAF].routers[router]++;
+
+        if (!acc[CAF].states[state]) {
+            acc[CAF].states[state] = 0;
+        }
+        acc[CAF].states[state]++;
+
+        acc[CAF].drivers.add(driver);
+        acc[CAF].boardings.add(boarding);
+
+        return acc;
+    }, {});
+
+    const result = {};
+
+    for (const [CAF, value] of Object.entries(groupedData)) {
+        const { AWBS, routers, states, drivers, boardings, ticket } = value;
+
+        // Determine the most frequent router
+        const mostFrequentRouter = Object.entries(routers).reduce((a, b) => (b[1] > a[1] ? b : a))[0];
+
+        // Determine the most frequent state
+        const stateCounts = { atTheBase: 0, inCAF: 0, inStreet: 0 };
+        for (const [state, count] of Object.entries(states)) {
+            if (atTheBase.includes(state)) {
+                stateCounts.atTheBase += count;
+            } else if (inCAF.includes(state)) {
+                stateCounts.inCAF += count;
+            } else if (inStreet.includes(state)) {
+                stateCounts.inStreet += count;
+            }
+        }
+
+        const mostFrequentState = Object.entries(stateCounts).reduce((a, b) => (b[1] > a[1] ? b : a))[0];
+        const stateValue = stateCounts[mostFrequentState] > 0 ? mostFrequentState : 'undefined';
+
+        result[CAF] = {
+            CAF: CAF,
+            AWBS: Array.from(AWBS),
+            router: mostFrequentRouter,
+            state: stateValue,
+            driver: Array.from(drivers)[0], // Assuming driver is the same for all entries in a CAF
+            boarding: Array.from(boardings),
+            ticket: ticket
+        };
+    }
+
+    return result;
 }
 
 //:-------------
@@ -268,5 +417,11 @@ function errorHandling(errMsg, error) {
     if (error) {
         console.error('Ocorreu um erro: ', error);
     }
+}
+function getStateAvailable(data) {
+    const mainDataObject = createObjectData(data);
+    const states = mainDataObject.map(item => item.state);
+    const uniqueStates = [...new Set(states)];
+    return uniqueStates;
 }
 //:-------------
